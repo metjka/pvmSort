47b61f56d6e744e94477031a5c7e2331007442c7
student <you@example.com>
2015-11-27 03:08:56 +0100

    fix

---
+++ sort_def.h
@@ -0,0 +1,9 @@
+#include "pvm3.h" // dla funkcji pvm_...
+#include <stdio.h> // printf
+#include <stdlib.h> // malloc
+#define TAG_MASTER 1 // tag wiadomości wysyłanych przez master
+#define TAG_SLAVE 2 // tag wiadomości wysyłanych przez slave
+#define TAG_MIN 3
+#define TAG_MAX 4
+#define GROUP_NAME "grupa"
+#define SLAVENUM 4
