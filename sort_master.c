2a6147c61ce8c4e0cf5a0425fce486c3a8aee63b
student <you@example.com>
2015-11-28 17:25:19 +0100

    final version

--- sort_master.c
+++ sort_master.c
@@ -1,30 +1,31 @@
 #include "sort_def.h"
 
 int main(int argc, char **argv) {
-    int nproc, i;
-    int tids[SLAVENUM], ok, mytid;
+	int nproc, status;
+	int tids[SLAVENUM], ok, mytid;
+	nproc = pvm_spawn("sort_slave",0,PvmTaskDefault, "LINUX64",SLAVENUM, tids);
+	printf("Master id %d\n", nproc );
 
-    nproc = pvm_spawn("sort_slave", 0, 0, "LINUX64", SLAVENUM, tids);
-    mytid = pvm_mytid();
-	
-    // 2. zbieranie info od slavow
-    int licznik = 0;
-    for (i = 0; i < nproc; i++) { // odbieranie wiadomości
-   
-        pvm_recv(-1, TAG_SLAVE);
-        pvm_upkint(&ok, 1, 1);
-        printf("Master: ok %d \n", ok);
-        if (ok == 1)
-            licznik++;
-    }
-    if (licznik == SLAVENUM) {  // wyslij wiadomosc do slaveow, ze jest koniec sortowania
+    do{
+    	status = 0;
+        //if 0 - all done
+        //if 1 - continue
+    	for(int i = 0; i < SLAVENUM; i++) {
+    		int slave_status ;
+    		pvm_recv (-1 , TAG_SLAVE);
+    		pvm_upkint(&slave_status, 1, 1);
+    		if (slave_status == 0){
+    			status = 1;
+    		}
+    	}
+    	for (int i = 0; i < SLAVENUM; ++i) {
+			pvm_initsend( PvmDataRaw );
+			pvm_pkint(&status, 1, 1);
+			pvm_send( tids[i], TAG_MASTER );    	
+		}
+	}while(status == 1);
 
-        for (i = 0; i < SLAVENUM; i++) {// wysłanie wiadomości
-        
-            pvm_initsend(PvmDataRaw);
-            pvm_send(tids[i], TAG_MASTER);
-        }
-    }
+
+    
 	pvm_exit();
-	return 0;
 }
--- sort_slave.c
+++ sort_slave.c
@@ -1,118 +1,123 @@
 #include "sort_def.h"
-
-int main()
-{
+int sortStatus();
+int main(){
 	int ptid, mytid; // parent TID, mój TID
 	int i; // wspólrzedne x i y wylosowanego punktu, iterator
 	int * pointsTab;
 	int points = 5;
-	srand( pvm_mytid() ); // wlacznie generacji liczb losowych
+	
 
-	// 1 - tworzy tablice z wartosciami
+	srand( pvm_mytid() ); // wlacznie generacji liczb losowych
+	printf("I am slave and my id %d \n", pvm_mytid());
+	int gid = pvm_joingroup(GROUP_NAME);
+	printf("Waiting on barrier \'%s\'\n",GROUP_NAME);
+	pvm_barrier(GROUP_NAME, SLAVENUM);
+	printf("Ok, let`s do this.\nMy groun id: %d\n", gid);
+	
+	// 1 - creating og list
 	pointsTab = (int *)malloc(5*sizeof(int));
 	for (i=0; i<5; i++) {
 		pointsTab[i] = rand()%100;
 		printf(" %d", pointsTab[i] );
+	}
+	printf("\n");
+    do{
+    	int status = 1;
+    	//1 nothing changed
+    	//0 sth changed
+    	printf("\n");
+    	for (i=0; i<5; i++) {
+			printf(" %d", pointsTab[i] );
+		}
 		printf("\n");
-	}
+		//min value 
+		int min = pointsTab[0];
+		int min_index = 0;
+		for (i=0; i<points; i++) {
+			if ( pointsTab[i] < min ){
+				min = pointsTab[i];
+				min_index = i;
+			}
+		}
+		//max value
+		int max = pointsTab[0];
+		int max_index = 0;
+		for (i = 0; i < points; i++){
+			if(pointsTab[i] > max){
+				max = pointsTab[i];
+				max_index = i;
+			}
+		}
+		printf("Slave: %d, Element min:%d \n", gid, min);
+		printf("Slave: %d, Element max:%d \n", gid, max);
 
-	// 2 - dolaczenie do grupy i poczekanie na koniec zadan
-	int ginst;
-	ginst = pvm_joingroup(GROUP_NAME);
-	printf("Czekam na barierze %s\n",GROUP_NAME);
+		// 4.1 sending min value <<<---
+		if (gid != 0) {
+			printf("Slave: %d sent MINVALUE to Slave: %d \n", gid, gid -1 );
+			int tid = pvm_gettid( GROUP_NAME, (gid -1) );
+			pvm_initsend(PvmDataDefault);
+			pvm_pkint(&min,1,1);
+			pvm_send(tid, TAG_MIN);
+		}
+		// 4.2 sending max value --->>>
+		if (gid != pvm_gsize( GROUP_NAME) - 1){
+			printf("Slave: %d sent MAXVALUE to Slave: %d \n",gid,  gid + 1 );
+
+			int tid = pvm_gettid( GROUP_NAME, (gid +1) );
+			pvm_initsend(PvmDataDefault);
+			pvm_pkint(&max,1,1);
+			pvm_send(tid, TAG_MAX);
+		}
+		//5.1 geting max from left
+		int max_sa;
+		if (gid > 0){
+			int tid = pvm_gettid( GROUP_NAME, gid - 1 );
+			pvm_recv(tid, TAG_MAX);
+			pvm_upkint(&max_sa, 1, 1);
+			printf("Slave: %d, got from %d MAX value: %d \n",gid, (gid -1), max_sa);
+			if (min < max_sa){
+				pointsTab[min_index] = max_sa;
+				status = 0;
+			}
+		}
+		//5.2 geting min from  right
+		int min_sa;
+		if (gid < pvm_gsize( GROUP_NAME ) -1){
+			int tid = pvm_gettid( GROUP_NAME, gid + 1 );
+			pvm_recv(tid, TAG_MIN);
+			pvm_upkint(&min_sa, 1, 1);
+			printf("Slave: %d, got from  %d MIN value: %d \n", gid, (gid +1), min_sa);
+			if (max > min_sa){
+				pointsTab[max_index] = min_sa;
+				status = 0;
+			}
+		}	
+	
+		pvm_initsend( PvmDataDefault);
+		pvm_pkint( &status, 1, 1);
+		pvm_send(pvm_parent(), TAG_SLAVE);
+
+		printf("---------\n");
+    } while(sortStatus()==1);
+
 	pvm_barrier(GROUP_NAME, SLAVENUM);
-	printf("Ok, zaczynam prace %d\n", ginst);
+	printf("Slave %d waiting on second barrier!\n",gid);
 
-	// 3.1 - wyszukanie najmniejszego elementu 
-	int min = pointsTab[0];
-	int ind_min = 0;
-	for (i=1; i<points; i++) {
-		if ( pointsTab[i] < min ) {
-			min = pointsTab[i];
-			ind_min = i;
-		}
+	for (i=0; i<5; i++) {
+			printf(" %d", pointsTab[i] );
 	}
-	printf("Slave: %d, Element minimalny:%d \n", ginst, min);
-	
-	// 3.2 - wyszukanie najwiekszego elementu 
-	int max = pointsTab[0];
-	int ind_max = 0;
-	for (i=1; i<points; i++) {
-		if ( pointsTab[i] > min ) {
-			max = pointsTab[i];
-			ind_max = i;
-		}
-	}
-	printf("Slave: %d, Element maxymalny:%d \n", ginst, max);
+	printf("\n");
+	printf("End of sorting!\n\n");
+    pvm_exit();    
+}
 
-	// 4.1 - przeslanie elementu min do sasiada po lewej stronie
-	if (ginst != 0) {
-	int tid = pvm_gettid( GROUP_NAME, ginst -1 );
-		pvm_initsend(PvmDataDefault);
-		pvm_pkint(&min,1,1);
-		pvm_send(tid, TAG_MIN);
-	}
-	
-	// 4.2 - przeslanie elementu max do sasiada po prawej stronie
-	if (ginst != pvm_gsize( GROUP_NAME ) -1) {
-	int tid = pvm_gettid( GROUP_NAME, ginst -1 );
-		pvm_initsend(PvmDataDefault);
-		pvm_pkint(&max,1,1);
-		pvm_send(tid, TAG_MAX);
-	}
 
-	int max_sa;
-	// 5.1 - czekanie na odpowiedz z elementem maks od sasiada
-	if (ginst != (pvm_gsize( GROUP_NAME )) -1){
-		pvm_recv(-1, TAG_MAX);
-		pvm_upkint(&max_sa, 1, 1);
-		printf("Slave: %d, odebral max: %d \n", ginst, max_sa);
-	}
-	int min_sa;
-	// 5.2 - czekanie na odpowiedz z elementem min od sasiada
-	if (ginst != 0){
-		pvm_recv(-1, TAG_MIN);
-		pvm_upkint(&min_sa, 1, 1);
-		printf("Slave: %d, odebral min: %d \n", ginst, min_sa);
-	}
-	
-	int ok;	
-	// 6 - podmienienie elementu min z maks
-	if(min < max_sa)
-	{
-		// podmiana 
-		pointsTab[ind_min] = max_sa;
-		ok = 0;
-	}
-	else 
-		ok=1;
+int sortStatus(){
+	int status = 0;
 
-	if( max > min_sa ) 
-	{
-		// podmiana
-		pointsTab[ind_max] = min_sa;
-		ok = 0;
-	}
-	else
-		ok=1;
-
-	ptid= pvm_parent();
-	
-	pvm_initsend( PvmDataDefault);
-	pvm_pkint( &ok, 1, 1);
-	pvm_send(ptid, TAG_SLAVE);
-
-    // 8 - odebranie od mastera infa czy koniec sortowania
-    int status = pvm_nrecv(-1, TAG_MASTER);
-    if(status >0){
-
-		printf("suc");
-        pvm_exit();
-		exit(0);
-
-            
-
+	if(pvm_recv(-1, TAG_MASTER)){
+		pvm_upkint(&status,1,1);
 
 	}
-	pvm_exit();
+	return status;
 }
